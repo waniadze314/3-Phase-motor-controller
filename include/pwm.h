467a844b9503bf354feac5ab402dd40a9876b604
Kamil <kamilkasjan@poczta.fm>
2020-09-11 09:49:00 +0200

    add current to pwm transformation

---
+++ include/pwm.h
@@ -0,0 +1,3 @@
+void init_pwm();
+void current_to_pwm_transform(float* current, float max_amplitude);
+void test_pwm();
---
+++ src/pwm.c
@@ -0,0 +1,48 @@
+#define PWM_U 12
+#define PWM_V 13
+#define PWM_W 14
+
+#include "driver/mcpwm.h"
+#include "pwm.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "soc/mcpwm_periph.h"
+
+void init_pwm(){
+    mcpwm_config_t pwm_config ={
+        .frequency = 20000,
+        .cmpr_a = 50.0,
+        .counter_mode = MCPWM_UP_COUNTER,
+        .duty_mode = MCPWM_DUTY_MODE_0,
+    };
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, PWM_U);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, PWM_V);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM2A, PWM_W);
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_2, &pwm_config);
+    mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_0); 
+    mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_1);
+    mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_2);
+}
+
+void current_to_pwm_transform(float* current, float max_amplitude){
+    float pwm_values[3];
+    for(uint8_t i = 0; i<3; i++){
+        pwm_values[i] = ((current[i] + (max_amplitude/2.0))/max_amplitude) * 100.0;
+        if(pwm_values[i] > 100.0) pwm_values[i] = 100.0;
+        else if(pwm_values[i] < 0.0) pwm_values[i] = 0.0;
+    }
+}
+
+void test_pwm(){
+    init_pwm();
+    while(1){
+        for(int i = 25; i<75; i++){
+            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i-20);
+            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, i);
+            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_2, MCPWM_OPR_A, i+20);
+            vTaskDelay(20/portTICK_RATE_MS);
+        }
+    }
+}
