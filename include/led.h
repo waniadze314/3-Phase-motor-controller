eaa1490fcf662e95d72092301233ee048e0a3fc7
Kamil <kamilkasjan@poczta.fm>
2020-11-20 08:40:49 +0100

    rename blink_led_task

--- include/led.h
+++ include/led.h
@@ -3,4 +3,4 @@
 #include "freertos/task.h"
 #define GPIO_LED 4
 void init_led_gpio();
-void blink_led();
+void blink_led_task();
--- src/main.c
+++ src/main.c
@@ -4,8 +4,10 @@
 #include "encoder.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "led.h"
 void app_main() {
-    xTaskCreatePinnedToCore(calculate_current_task, "CURRENT", 1024, NULL, 2, NULL, 0);
-    xTaskCreatePinnedToCore(communication_task, "COMMUNICATION", 1024, NULL, 0, NULL, 0);
-    xTaskCreatePinnedToCore(encoder_task, "ENCODER", 1024, NULL, 0, NULL, 1);
+    // xTaskCreatePinnedToCore(calculate_current_task, "CURRENT", 1024, NULL, 2, NULL, 0);
+    // xTaskCreatePinnedToCore(communication_task, "COMMUNICATION", 1024, NULL, 1, NULL, 0);
+    // xTaskCreatePinnedToCore(encoder_task, "ENCODER", 1024, NULL, 0, NULL, 1);
+    xTaskCreatePinnedToCore(blink_led_task, "LED", 2048, NULL, 0, NULL, 0);
 }
