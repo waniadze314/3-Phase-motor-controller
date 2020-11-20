8d448734422a346fb17dca2e13b07704f5e54030
Kamil <kamilkasjan@poczta.fm>
2020-11-20 11:36:51 +0100

    prepare to merge

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 #include "freertos/task.h"
 #include "led.h"
 void app_main() {
-    xTaskCreatePinnedToCore(calculate_current_task, "CURRENT", 1024, NULL, 2, NULL, 0);
-    xTaskCreatePinnedToCore(communication_task, "COMMUNICATION", 1024, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(calculate_current_task, "CURRENT", 4096, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(communication_task, "COMMUNICATION", 4096, NULL, 0, NULL, 0);
     // xTaskCreatePinnedToCore(encoder_task, "ENCODER", 4096, NULL, 0, NULL, 1);
 }
 }
