ff011fdb10e71abd4db930c23b05f890d1c0ad17
Kamil <kamilkasjan@poczta.fm>
2020-11-20 11:36:18 +0100

    prepare to merge

--- src/main.c
+++ src/main.c
@@ -8,4 +8,6 @@
 void app_main() {
     xTaskCreatePinnedToCore(calculate_current_task, "CURRENT", 1024, NULL, 2, NULL, 0);
     xTaskCreatePinnedToCore(communication_task, "COMMUNICATION", 1024, NULL, 1, NULL, 0);
+    // xTaskCreatePinnedToCore(encoder_task, "ENCODER", 4096, NULL, 0, NULL, 1);
+}
 }
